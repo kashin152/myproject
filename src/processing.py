b98f70ad27b4356bd8f60e25fadf3953099a95c2
alexandra kashina <kashin159@gmail.com>
2024-05-12 00:17:12 +0300

    change processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+"""Функция, которая принимает на вход список словарей и значение для ключа state (опциональный параметр со значением по умолчанию EXECUTED) и возвращает новый список, содержащий только те словари, у которых ключ state содержит переданное в функцию значение."""
 def filter_by_state(input_list: list, state: str = "EXECUTED") -> list:
     new_input_list = []
     for item in input_list:
@@ -6,6 +7,7 @@ def filter_by_state(input_list: list, state: str = "EXECUTED") -> list:
     return new_input_list
 
 
+"""Функция которая принимает на вход список словарей и возвращает новый список, в котором исходные словари отсортированы по убыванию даты (ключ date). zФункция принимает два аргумента, второй необязательный задает порядок сортировки (убывание, возрастание)."""
 def sort_by_state(input_list: list, order: str = "desc") -> list:
     return sorted(input_list, key=lambda x: x["date"], reverse=True)
 
