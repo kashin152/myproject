b3cee6c45613ecd8a69d93cecdee48071026e2cf
alexandra kashina <kashin159@gmail.com>
2024-06-10 11:39:43 +0300

    изменение файла utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,11 @@
 import json
 import os
-from typing import Any, List, Dict, Union
+from typing import List
 
 from src.external_api import currency_conversion
 
 
-def transaction_amount(list_transactions: List[Dict[str, Any]]) -> float:
-    """Функция, которая выводит сумму транзакции"""
-    if not list_transactions:
-        print("Транзакций не обнаружено.")
-
-    for transaction in list_transactions:
-        if "operationAmount" in transaction:
-            currency = transaction["operationAmount"]["currency"]["code"]
-            if currency == "RUB":
-                print(transaction["operationAmount"]["amount"])
-            elif currency != "RUB":
-                print(currency_conversion(currency, transaction["operationAmount"]["amount"]))
-
-
-def data_transactions(way: str) -> Union[bool, List[Dict[Any, Any]]]:
+def data_transactions(way: str) -> List[dict]:
     """
     Функция, которая принимает на вход путь до JSON-файла и
     возвращает список словарей с данными о финансовых транзакциях.
@@ -31,12 +17,31 @@ def data_transactions(way: str) -> Union[bool, List[Dict[Any, Any]]]:
                 return list_data_transactions
             except json.decoder.JSONDecodeError:
                 print("Ошибка обработки файла")
-                return False
+                return []
     except FileNotFoundError:
         return []  # instead of return False
 
-    return list_data_transactions
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 json_file_path = os.path.join(current_dir, "../data", "operations.json")
-transactions = data_transactions(json_file_path)
+list_transactions = data_transactions(json_file_path)
+
+
+def transaction_amount(txn: dict) -> float:
+    """Функция, которая выводит сумму транзакции"""
+    if not txn:
+        return 0.0
+
+    if "operationAmount" in txn:
+        currency = txn["operationAmount"]["currency"]["code"]
+        if currency == "RUB":
+            return txn["operationAmount"]["amount"]
+        elif currency != "RUB":
+            return currency_conversion(currency, txn["operationAmount"]["amount"])
+
+    return 0.0
+
+
+if __name__ == '__main__':
+    for transaction in list_transactions:
+        print(transaction_amount(transaction))
